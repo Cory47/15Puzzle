7155cddc009b371f4dc6974d7da8cf195de3de9b
Cory47 <marleau24@up.edu>
2021-09-26 15:14:14 -0700

    Project completed and documented

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -3,9 +3,31 @@ package com.example.homework2;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ *  Main Activity: finds the elements from activity_main.xml
+ *  and creates listeners for them in GameSurfaceView.java
+ *
+ * @author Cory Marleau
+ * @version September 26 2021
+ *
+ *  Enhancements: This project has +/- buttons that can change
+ *  the size of the board. It can be reduced from a 3x3 board or
+ *  increased to a 9x9.
+ *
+ *  The size of the tiles changes based on the screen size, and will resize
+ *  to take up as much space as possible. The tiles are drawn with a surfaceView
+ *  instead of using buttons
+ *
+ *  Bugs: Small paint error upon resizing the board. Some tiles that are
+ *  in their correct position do not change color. However it is immediately
+ *  resolved when the board is tapped. Not game breaking. Very minimal.
+ *
+ *  No other known bugs.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,7 +38,14 @@ public class MainActivity extends AppCompatActivity {
         //Step 1: Find the object of elements to interact
         GameSurfaceView gameSurfaceView = (GameSurfaceView) findViewById(R.id.gameSurfaceView);
 
+        View increaseSizeButton = (View) findViewById(R.id.sizeUpButton);
+        View decreaseSizeButton = (View) findViewById(R.id.sizeDownButton);
+        View shuffleButton = (View) findViewById(R.id.shuffleButton);
+
         //Step 2: Set up listeners for the objects
         gameSurfaceView.setOnTouchListener(gameSurfaceView);
+        increaseSizeButton.setOnClickListener(gameSurfaceView);
+        decreaseSizeButton.setOnClickListener(gameSurfaceView);
+        shuffleButton.setOnClickListener(gameSurfaceView);
     }
 }
